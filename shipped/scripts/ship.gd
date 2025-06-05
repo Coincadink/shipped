e66ce95467133a0f548aea608fd8ffa19380d7c0
John Peterson <johnp5419@gmail.com>
2025-06-04 23:53:02 -0600

    FINALLY FIXED

--- shipped/scripts/ship.gd
+++ shipped/scripts/ship.gd
@@ -1,4 +1,14 @@
 extends RigidBody2D
 
+var previous_velocity = Vector2.ZERO
+var acceleration = Vector2.ZERO
+
 func _physics_process(delta: float) -> void:
 	global_rotation = linear_velocity.angle() + PI/2
+	
+	var current_velocity = linear_velocity
+	acceleration = (current_velocity - previous_velocity) / delta
+	previous_velocity = current_velocity
+
+	var mag = acceleration.length() / 1000
+	set_modulate(Color(mag, 0, 0, 1))
